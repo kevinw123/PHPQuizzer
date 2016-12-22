ae5ada9fcac7abf8c026da75fd7b0bc35be991ae
kevinw123 <kevinwong_895@hotmail.com>
2016-12-21 21:03:37 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>PHP Quizzer</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<h1>PHP Quizzer</h1>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<h2>Test Your PHP Knowledge</h2>
+			</div>
+		</main>
+		<footer>
+			<div class="container">
+				Copyright &copy; 2016, PHP Quizzer
+			</div>
+		</footer>
+	</body>
+</html>
