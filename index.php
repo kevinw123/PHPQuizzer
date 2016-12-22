40275ff7ce087b78d50b231a1f9c116251f3abb4
kevinw123 <kevinwong_895@hotmail.com>
2016-12-21 21:22:20 -0800

    Adding UI for question page

--- index.php
+++ index.php
@@ -14,6 +14,13 @@
 		<main>
 			<div class="container">
 				<h2>Test Your PHP Knowledge</h2>
+				<p>This is a multiple choice quiz to test your knowlege of PHP</p>
+				<ul>
+					<li><strong>Number of Questions: </strong>5</li>
+					<li><strong>Type: </strong>Multiple Choice</li>
+					<li><strong>Estimated Time: </strong>4 Minutes</li>
+				</ul>
+				<a href="question.php?n=1" class="start">Start Quiz</a>
 			</div>
 		</main>
 		<footer>
